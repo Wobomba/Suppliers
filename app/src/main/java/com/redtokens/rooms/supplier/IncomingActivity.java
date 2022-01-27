3fe874ce8a28766bfc4f06d2379025e93ccb3480
Wobomba <iwobomba999@gmail.com>
2022-01-27 15:14:15 +0300

    New Patch1

--- app/src/main/java/com/redtokens/rooms/supplier/IncomingActivity.java
+++ app/src/main/java/com/redtokens/rooms/supplier/IncomingActivity.java
@@ -2,13 +2,43 @@ package com.redtokens.rooms.supplier;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.redtokens.rooms.supplier.ui.orders.OrdersFragment;
 
 public class IncomingActivity extends AppCompatActivity {
-
+    EditText Cat,Bname,Dest,Amount,P_Status;
+    Button acc, decl;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_incoming);
+
+        Cat = findViewById(R.id.Category_Outgoing);
+        Bname = findViewById(R.id.Cat_Outgoing);
+        Dest = findViewById(R.id.Outgoing_name);
+        Amount = findViewById(R.id.Amount_outgoing);
+        P_Status = findViewById(R.id.Destination_outgoing);
+        acc = findViewById(R.id.accept_btn);
+        decl = findViewById(R.id.decline_btn);
+
+        decl.setOnClickListener(view -> {
+            Intent intent = new Intent(IncomingActivity.this,FeedbackActivity.class);
+            startActivity(intent);
+            finish();
+        });
+
+        acc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(IncomingActivity.this, OrdersFragment.class);
+                startActivity(i);
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/redtokens/rooms/supplier/ui/client/ClientFragment.java
+++ app/src/main/java/com/redtokens/rooms/supplier/ui/client/ClientFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TableLayout;
+
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
@@ -11,10 +13,15 @@ import androidx.fragment.app.Fragment;
 
 import com.redtokens.rooms.supplier.R;
 
+import java.util.ArrayList;
+
 public class ClientFragment extends Fragment {
-
-
-
+    private ArrayList<String> client_name = new ArrayList<String>();
+    private ArrayList<String> item = new ArrayList<String>();
+    private ArrayList<String> category = new ArrayList<String>();
+    private ArrayList<String> destination = new ArrayList<String>();
+    private ArrayList<String> status = new ArrayList<String>();
+    TableLayout table;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
