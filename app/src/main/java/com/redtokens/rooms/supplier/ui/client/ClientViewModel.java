4e42592c9cb81205f70d553bd569da77e6b162c5
Wobomba <iwobomba999@gmail.com>
2022-01-27 13:19:10 +0300

    New Patch0

--- app/src/main/java/com/redtokens/rooms/supplier/ui/client/ClientViewModel.java
+++ app/src/main/java/com/redtokens/rooms/supplier/ui/client/ClientViewModel.java
@@ -1,14 +1,14 @@
-package com.redtokens.rooms.supplier.ui.home;
+package com.redtokens.rooms.supplier.ui.client;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class HomeViewModel extends ViewModel {
+public class ClientViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
 
-    public HomeViewModel() {
+    public ClientViewModel() {
         mText = new MutableLiveData<>();
         mText.setValue("This is home fragment");
     }
